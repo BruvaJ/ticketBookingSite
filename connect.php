9f1058a76ce7e161c01ade17d41967179b50e620
John <bruvajohn@gmail.com>
2016-06-15 18:04:22 +0100

    password edited out

--- connect.php
+++ connect.php
@@ -7,7 +7,8 @@
 			$host = "dragon.ukc.ac.uk";
 			$dbuser = "jww21";
 			$dbname = "jww21";
-			$dbpass = "rhagev!";
+			//edited out for privacy
+			$dbpass = "******";
 			$conn = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $dbpass);
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			if ($conn) {
